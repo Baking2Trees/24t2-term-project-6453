910216df4a4704d6705d24ad4031e7befedeb1b4
Joshua Louttit <jlouttit@outlook.com>
2024-07-29 16:42:40 +1000

    construction of merkle tree object

---
+++ merkle.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include "merkle.h"
+#include "constants.h"
+
+merkle_tree::merkle_tree(char* filename) {
+    FILE* file = std::fopen(filename, "rb");
+    if (!file) {
+        std::cerr << "Could not open the file.\n";
+        std::exit(1);
+    }
+    
+    // // Move file pointer to store files size
+    // std::fseek(file, 0, SEEK_END);
+    // long fileSize = std::ftell(file);
+    // std::fseek(file, 0, SEEK_SET); // Dont need this atm
+
+    unsigned char data[BLOCK_SIZE];
+
+    // Construct all child nodes
+    std::vector<std::shared_ptr<merkle_node>> nodes;
+    int bytes_read;
+    int block_number = 0;
+    while ((bytes_read = fread(data, 1, BLOCK_SIZE, file)) != 0) {
+        std::string hash = SHA256_hash_as_string(data, bytes_read);
+        
+        // New shared pointer to child node
+        std::shared_ptr<merkle_node> new_child(new merkle_node(hash, block_number));
+        nodes.push_back(new_child);
+        block_number++;
+    }
+
+    // Merge to root
+    while (nodes.size() != 1) {
+        // Merge in pairs first
+        int i = 0;
+        std::vector<std::shared_ptr<merkle_node>> tmp;
+        while (i + 1 < nodes.size()) {
+            // New node
+            std::shared_ptr<merkle_node> new_node(new merkle_node(nodes[i], nodes[i + 1]));
+            tmp.push_back(new_node);
+
+            i = i + 2;
+        }
+
+        // Merge last if present
+        if (nodes.size() % 2 != 0) {
+            tmp.push_back(nodes[i]);
+        }
+
+        nodes = tmp;
+    }
+
+    this->root_node_ = nodes[0];
+}
--- merkle.h
+++ merkle.h
@@ -2,16 +2,40 @@
 #define MERKLE_H
 
 #include <string>
+#include "sha256.h"
 
 class merkle_node {
+ public:
+    // Class attributes
+    std::string hash_;
+    std::shared_ptr<merkle_node> left_child_;
+    std::shared_ptr<merkle_node> right_child_;
+    int leftmost_block_;
+    int rightmost_block_;
+    
+    // Childnode
+    merkle_node(std::string hash, int block_number)
+    : hash_(hash)
+    , left_child_(nullptr)
+    , right_child_(nullptr)
+    , leftmost_block_(block_number)
+    , rightmost_block_(block_number){};
 
+    // Merging of two childnodes
+    merkle_node(std::shared_ptr<merkle_node> left_child, std::shared_ptr<merkle_node> right_child)
+    : hash_(merkle_node_sha256_hash(left_child->hash_, right_child->hash_))
+    , left_child_(left_child)
+    , right_child_(right_child)
+    , leftmost_block_(left_child->leftmost_block_)
+    , rightmost_block_(right_child->rightmost_block_){};
 };
 
 class merkle_tree { 
+ public:
     // Only valid constructor - creates a merkle tree with a given filename
-    explicit merkle_tree(char *filename);
+    merkle_tree(char *filename);
 
-    std::string root_hash;
+    std::shared_ptr<merkle_node> root_node_;
 };
 
 
