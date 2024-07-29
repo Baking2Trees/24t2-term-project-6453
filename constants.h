8b6c965fcf40af04729cc725b6ff6bf35acab8af
Joshua Louttit <jlouttit@outlook.com>
2024-07-29 16:43:35 +1000

    file to contain necessary constants

---
+++ constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define HASH_SIZE 32 // 32 bytes is a sha256 hash
+#define BLOCK_SIZE 256 // Defined block byte size of file. 256 is large but not too large
+
+#endif // CONSTANTS_H
