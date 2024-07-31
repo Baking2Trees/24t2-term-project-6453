abb7d96933ff40fe873992de07b64ab811dbd1e6
Joshua Louttit <jlouttit@outlook.com>
2024-07-31 15:15:22 +1000

    moved implementation to cpp file

--- constants.h
+++ constants.h
@@ -7,13 +7,6 @@
 #define HASH_SIZE 32 // 32 bytes is a sha256 hash
 #define BLOCK_SIZE 256 // Defined block byte size of file. 256 is large but not too large
 
-std::string request_blocks_file_name(std::vector<int> missing_blocks, char *filename) {
-    std::string return_string = std::string{"requestForBlocks"};
-    for (int i = 0; i < missing_blocks.size(); i++) {
-        return_string += missing_blocks[i];
-    }
-    return_string = return_string + "ForFile" + filename;
-    return return_string;
-}
+std::string request_blocks_file_name(std::vector<int> missing_blocks, char *filename);
 
 #endif // CONSTANTS_H
