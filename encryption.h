fde3f6a6b312876a5a754d9e128aa6771f5e680f
jameshanly2001 <91174802+jameshanly2001@users.noreply.github.com>
2024-08-07 13:02:28 +1000

    Update encryption.h

--- encryption.h
+++ encryption.h
@@ -1,19 +1,26 @@
-#ifndef ENCRYPT_AND_SEND_H
-#define ENCRYPT_AND_SEND_H
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
 
 #include <string>
 #include <vector>
-#include <openssl/rsa.h>
 
+// Load RSA public key from a PEM-formatted string
+RSA* load_public_key(const std::string& public_key_str);
 
+// Encrypt data using RSA public key
+std::string encrypt_data(RSA* rsa, const std::string& data);
+
+// Load RSA private key from a PEM-formatted string
 RSA* load_private_key(const std::string& private_key_str);
 
+// Sign data using RSA private key
 std::vector<unsigned char> sign_data(RSA* rsa, const std::string& data);
 
+// Encrypt and send data
 void EncryptAndSend(const std::string& requestedData, 
                     const std::string& requesterPublicKey, 
                     const std::string& senderPrivateKey, 
                     const std::string& encryptedDataFilename, 
                     const std::string& signatureFilename);
 
-#endif // ENCRYPT_AND_SEND_H
+#endif // ENCRYPTION_H
